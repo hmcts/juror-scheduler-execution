ad82c1d6f9df63aaa151e202afb6aa1976ebd2e4
benedwards <ben.edwards@cgi.com>
2024-03-21 14:54:28 +0000

    Updated procedure name for content stores

--- src/main/java/uk/gov/hmcts/juror/job/execution/jobs/contentstore/payment/PaymentFileJob.java
+++ src/main/java/uk/gov/hmcts/juror/job/execution/jobs/contentstore/payment/PaymentFileJob.java
@@ -20,7 +20,7 @@ public class PaymentFileJob extends ContentStoreFileJob {
             paymentConfig.getFtpDirectory(),
             paymentConfig.getDatabase(),
             "PAYMENT",
-            "payment_files_to_clob_extract",
+            "payment_files_to_clob",
             new Object[0],
             "JURY\\d+\\.\\d+.*",
             PaymentSftp.class);
