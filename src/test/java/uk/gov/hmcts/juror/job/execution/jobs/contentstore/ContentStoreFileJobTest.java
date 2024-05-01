21bb941bb7146bf96c7bedea96c5133a3a0a6692
Taherah Sarwar <taherah.sarwar@cgi.com>
2024-05-01 11:38:40 +0100

    task/JM-7183 Code review

--- src/test/java/uk/gov/hmcts/juror/job/execution/jobs/contentstore/ContentStoreFileJobTest.java
+++ src/test/java/uk/gov/hmcts/juror/job/execution/jobs/contentstore/ContentStoreFileJobTest.java
@@ -235,12 +235,12 @@ public class ContentStoreFileJobTest {
                 FileSearch fileSearch = mock(FileSearch.class);
                 fileSearchMock.when(() -> FileSearch.directory(ftpDirectory, true)).thenReturn(fileSearch);
 
-                File file1 = Mockito.mock(File.class);
-                Mockito.when(file1.getName()).thenReturn("sample1.txt");
-                File file2 = Mockito.mock(File.class);
-                Mockito.when(file2.getName()).thenReturn("sample2.txt");
-                File file3 = Mockito.mock(File.class);
-                Mockito.when(file3.getName()).thenReturn("sample3.txt");
+                File file1 = mock(File.class);
+                when(file1.getName()).thenReturn("sample1.txt");
+                File file2 = mock(File.class);
+                when(file2.getName()).thenReturn("sample2.txt");
+                File file3 = mock(File.class);
+                when(file3.getName()).thenReturn("sample3.txt");
                 Set<File> files = new HashSet<>(Set.of(file1, file2, file3));
 
                 when(fileSearch.search()).thenReturn(files);
@@ -263,9 +263,6 @@ public class ContentStoreFileJobTest {
 
                 verify(fileSearch, times(1)).setFileNameRegexFilter(fileNameRegex);
                 for (File file : files) {
-                    String fileName = file.getName();
-                    when(file.getName()).thenReturn(fileName);
-
                     fileUtilsMock.verify(() -> FileUtils.deleteFile(eq(file)), times(1));
                     verify(sftpService, times(1)).upload(sftpClass, file);
                     verify(databaseService, times(1))
@@ -285,12 +282,12 @@ public class ContentStoreFileJobTest {
                 FileSearch fileSearch = mock(FileSearch.class);
                 fileSearchMock.when(() -> FileSearch.directory(ftpDirectory, true)).thenReturn(fileSearch);
 
-                File file1 = Mockito.mock(File.class);
-                Mockito.when(file1.getName()).thenReturn("sample1.txt");
-                File file2 = Mockito.mock(File.class);
-                Mockito.when(file2.getName()).thenReturn("sample2.txt");
-                File file3 = Mockito.mock(File.class);
-                Mockito.when(file3.getName()).thenReturn("sample3.txt");
+                File file1 = mock(File.class);
+                when(file1.getName()).thenReturn("sample1.txt");
+                File file2 = mock(File.class);
+                when(file2.getName()).thenReturn("sample2.txt");
+                File file3 = mock(File.class);
+                when(file3.getName()).thenReturn("sample3.txt");
                 Set<File> files = new HashSet<>(Set.of(file1, file2, file3));
 
                 when(fileSearch.search()).thenReturn(files);
@@ -299,10 +296,8 @@ public class ContentStoreFileJobTest {
 
                 SQLException sqlException = new SQLException("Forced sql exception");
                 for (File file : files) {
-                    String fileName = file.getName();
-                    when(file.getName()).thenReturn(fileName);
                     fileUtilsMock.when(() ->
-                            databaseService.executeUpdate(connection, UPDATE_SQL_QUERY, fileName, fileType))
+                            databaseService.executeUpdate(connection, UPDATE_SQL_QUERY, file.getName(), fileType))
                         .thenThrow(sqlException);
                 }
 
