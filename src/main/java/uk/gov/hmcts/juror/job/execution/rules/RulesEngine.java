81d28f4616edfbac0cb697d53c4e668858a35eab
akikrahman1 <149579836+akikrahman1@users.noreply.github.com>
2026-06-18 09:27:53 +0100

    Update RulesEngine.java
    fix issue where test can fail randomly as shared state is mutated

--- src/main/java/uk/gov/hmcts/juror/job/execution/rules/RulesEngine.java
+++ src/main/java/uk/gov/hmcts/juror/job/execution/rules/RulesEngine.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.juror.job.execution.rules;
 
 import jakarta.validation.constraints.NotNull;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -13,12 +12,9 @@ public final class RulesEngine {
     }
 
     public static List<String> fire(@NotNull Collection<Rule> rules) {
-        List<String> failureMessages = new ArrayList<>();
-        rules.parallelStream().forEach(rule -> {
-            if (!rule.execute()) {
-                failureMessages.add(rule.getMessage());
-            }
-        });
-        return failureMessages;
+        return rules.parallelStream()
+            .filter(rule -> !rule.execute())
+            .map(Rule::getMessage)
+            .toList();
     }
 }
