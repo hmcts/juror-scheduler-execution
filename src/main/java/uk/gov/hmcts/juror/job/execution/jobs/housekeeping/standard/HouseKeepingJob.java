7ea1d1624f1f2495903021adb24e23a3d6fd8303
EPatterson1 <140503822+EPatterson1@users.noreply.github.com>
2026-09-25 17:04:18 +0100

    JS-1091 Update HouseKeepingJob to call deactivate inactive user (#331)
    * JS-1091 Update HouseKeepingJob to call deactive inactive user
    
    * JS-1091 CVE-2026-41707

--- src/main/java/uk/gov/hmcts/juror/job/execution/jobs/housekeeping/standard/HouseKeepingJob.java
+++ src/main/java/uk/gov/hmcts/juror/job/execution/jobs/housekeeping/standard/HouseKeepingJob.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.juror.job.execution.jobs.housekeeping.standard;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.juror.job.execution.jobs.Job;
 import uk.gov.hmcts.juror.job.execution.jobs.StoredProcedureJob;
 import uk.gov.hmcts.juror.job.execution.service.contracts.DatabaseService;
 
@@ -11,8 +12,24 @@ import uk.gov.hmcts.juror.job.execution.service.contracts.DatabaseService;
 @Setter
 public class HouseKeepingJob extends StoredProcedureJob {
 
+    static final String HOUSEKEEPING_PROCEDURE = "juror_mod.housekeeping_process";
+    static final String DEACTIVATE_INACTIVE_USERS_PROCEDURE = "juror_mod.deactivate_inactive_users";
+
+    private final int inactiveUserThresholdMonths;
+
     protected HouseKeepingJob(DatabaseService databaseService, HouseKeepingConfig config) {
-        super(databaseService, config.getDatabase(), "juror_mod.housekeeping_process",
+        super(databaseService, config.getDatabase(), HOUSEKEEPING_PROCEDURE,
             config.getMaxTimeout(), config.getOwnerRestrict());
+        this.inactiveUserThresholdMonths = config.getInactiveUserThresholdMonths();
+    }
+
+    @Override
+    public Job.Result executeStoredProcedure() {
+        this.databaseService.execute(getDatabaseConfig(), connection -> {
+            this.databaseService.executeStoredProcedure(connection, HOUSEKEEPING_PROCEDURE, procedureArguments);
+            this.databaseService.executeStoredProcedure(connection, DEACTIVATE_INACTIVE_USERS_PROCEDURE,
+                inactiveUserThresholdMonths);
+        });
+        return Job.Result.passed();
     }
 }
--- src/test/java/uk/gov/hmcts/juror/job/execution/jobs/housekeeping/standard/HouseKeepingJobTest.java
+++ src/test/java/uk/gov/hmcts/juror/job/execution/jobs/housekeeping/standard/HouseKeepingJobTest.java
@@ -2,14 +2,30 @@ package uk.gov.hmcts.juror.job.execution.jobs.housekeeping.standard;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomUtils;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import uk.gov.hmcts.juror.job.execution.config.DatabaseConfig;
+import uk.gov.hmcts.juror.job.execution.jobs.Job;
+import uk.gov.hmcts.juror.job.execution.model.Status;
 import uk.gov.hmcts.juror.job.execution.service.contracts.DatabaseService;
 import uk.gov.hmcts.juror.job.execution.testsupport.AbstractStoredProcedureJobTest;
+
+import java.sql.Connection;
+import java.util.function.Consumer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @Slf4j
 class HouseKeepingJobTest
     extends AbstractStoredProcedureJobTest<HouseKeepingJob, HouseKeepingConfig> {
 
-    private static final String PROCEDURE_NAME = "juror_mod.housekeeping_process";
+    private static final int INACTIVE_USER_THRESHOLD_MONTHS = 6;
+    private static final String PROCEDURE_NAME = HouseKeepingJob.HOUSEKEEPING_PROCEDURE;
 
     protected HouseKeepingJobTest() {
         super(PROCEDURE_NAME);
@@ -27,11 +43,49 @@ class HouseKeepingJobTest
     public HouseKeepingConfig createConfig() {
         HouseKeepingConfig config = new HouseKeepingConfig();
         config.setMaxTimeout(RandomUtils.nextInt());
+        config.setInactiveUserThresholdMonths(INACTIVE_USER_THRESHOLD_MONTHS);
         return config;
     }
 
     @Override
     protected Object[] getProcedureArguments(HouseKeepingConfig config) {
         return new Object[]{config.getMaxTimeout(), config.getOwnerRestrict()};
+    }
+
+    @Override
+    protected void verifyAdditionalStoredProcedures(DatabaseService databaseService, Connection connection,
+                                                    HouseKeepingJob job, HouseKeepingConfig config) {
+        verify(databaseService, times(1))
+            .executeStoredProcedure(connection, HouseKeepingJob.DEACTIVATE_INACTIVE_USERS_PROCEDURE,
+                config.getInactiveUserThresholdMonths());
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void positiveExecuteStoredProcedureRunsHousekeepingAndInactiveUserDeactivation() {
+        DatabaseService databaseService = mock(DatabaseService.class);
+        DatabaseConfig databaseConfig = mock(DatabaseConfig.class);
+        HouseKeepingConfig config = createConfig();
+        config.setDatabase(databaseConfig);
+        HouseKeepingJob job = createStoredProcedureJob(databaseService, config);
+
+        Job.Result result = job.executeStoredProcedure();
+
+        assertEquals(Status.SUCCESS, result.getStatus(), "Status should be SUCCESS");
+        assertNull(result.getMessage(), "ErrorMessage should be null");
+        assertNull(result.getThrowable(), "Exception should be null");
+        assertEquals(0, result.getMetaData().size(), "MetaData should be empty");
+
+        ArgumentCaptor<Consumer<Connection>> connectionConsumerCaptor = ArgumentCaptor.forClass(Consumer.class);
+        verify(databaseService, times(1)).execute(eq(databaseConfig), connectionConsumerCaptor.capture());
+
+        Connection connection = mock(Connection.class);
+        connectionConsumerCaptor.getValue().accept(connection);
+
+        verify(databaseService, times(1))
+            .executeStoredProcedure(connection, HouseKeepingJob.HOUSEKEEPING_PROCEDURE, job.getProcedureArguments());
+        verify(databaseService, times(1))
+            .executeStoredProcedure(connection, HouseKeepingJob.DEACTIVATE_INACTIVE_USERS_PROCEDURE,
+                config.getInactiveUserThresholdMonths());
     }
 }
